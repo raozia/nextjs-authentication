354feb73188af82c6cf7c8481d50cc484a9575c2
raozia <raozia.4433@gmail.com>
2026-08-19 20:32:46 +0500

    Update navbar styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 const NavBar = () => {
     return (
         <div>
-            <div className='flex justify-between'>
+            <div className='flex justify-between py-2 px-4'>
                 <div>
                     <h1>LOGO</h1>
                 </div>
                 <div>
-                    <div>
+                    <div className='flex gap-4'>
                         <Link href={'/#'}>Notification</Link>
                         <Link href={'/#'}>Cart</Link>
                         <Link href={'/#'}>Logout</Link>
